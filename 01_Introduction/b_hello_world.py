759cf6c480e8b2cf01645fd72a87e46fb05615d4
pkcommits <pratyushakaranam1@gmail.com>
2024-11-25 02:40:40 +0000

    class02 - introduction to python

--- 01_Introduction/b_hello_world.py
+++ 01_Introduction/b_hello_world.py
@@ -1,1 +1,18 @@
-print('Hello world')
+"""
+
+Purpose: Basic PEP 8 guideline and
+         shebang line
+
+    PEP - Python Enhancement Proposal
+    pep 8 - coding style guide
+
+This is doc string
+
+"""
+#print asa statement in python 2
+# print "hello world!"
+
+#print as a function in python 2 & 3 
+print("hello world!")
+print(True)
+print(True, 123, None)
