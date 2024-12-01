d83e0ee498048f7716129e3a1f78c87fcd51aa45
pkcommits <pratyushakaranam1@gmail.com>
2024-12-01 16:16:10 +0000

    class notes

--- 01_Introduction/b_hello_world.py
+++ 01_Introduction/b_hello_world.py
@@ -1,18 +1,38 @@
+#!/usr/bin/python3
+"""
+Purpose: Basic PEP 8 guidelines and 
+       shebang line
+
+
+    PEP - Python Enhancement Prosopal
+    PEP 8 - coding style guide
+
+This is docstring
+
 """
 
-Purpose: Basic PEP 8 guideline and
-         shebang line
+# print as a statement in python 2
+# print "Hello world!"
 
-    PEP - Python Enhancement Proposal
-    pep 8 - coding style guide
-
-This is doc string
-
-"""
-#print asa statement in python 2
-# print "hello world!"
-
-#print as a function in python 2 & 3 
-print("hello world!")
+# print as a function in python  2 & 3
+print("Hello world!")
 print(True)
-print(True, 123, None)
+print("True", 123, None)
+
+
+
+
+def wishes(name):
+    wish = "How are you {0}?".format(name)
+    print(wish)
+
+
+wishes("Udhay")
+
+
+def వందనాలు(పేరు):
+    వందనం = "మీరు ఎలా ఉన్నారు {0}?".format(పేరు)
+    print(వందనం)
+
+
+వందనాలు("పేరు")
